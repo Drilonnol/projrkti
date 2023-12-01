de55e2e2086cc5414fa30e8e835f524edf4ccfdc
Drilonnol <150484873+Drilonnol@users.noreply.github.com>
2023-12-01 10:39:40 +0100

    ndryshimt

---
+++ document.getElementById('loginn').js
@@ -0,0 +1,4 @@
+document.getElementById('loginn').addEventListener('click', function() {
+    var loginFormContainer = document.getElementById('loginFormContainer');
+    loginFormContainer.classList.toggle('active');
+});
