bf646da04804704df8fb4cb025ae26efb5482562
argjend780 <151842019+argjend780@users.noreply.github.com>
2023-12-06 23:07:23 +0100

    Slide show

--- scripp.js
+++ scripp.js
@@ -103,3 +103,22 @@ document.getElementById('cancelBtnSingin').addEventListener('click',function(){
 var registerContainer = document.getElementById('registerContainer');
 registerContainer.classList.remove('active');
 });
+
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change slide every 2 seconds
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    showSlides();
+});
