847ceb9a5c19aba4abd1ab4fb1c2eb858a2572c5
argjend780 <151842019+argjend780@users.noreply.github.com>
2024-01-23 21:36:31 +0100

    lidhja

--- Lidhja.php
+++ Lidhja.php
@@ -1,15 +1,33 @@
 <?php
-    $servername = "root";
-    $username = "root@localhost";
-    $password = "";
-    $dbname = "wizzair";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
+  class DatabaseConnection{
     
-    if ($conn->connect_error) {
-        die("Lidhja deshtoi: " . $conn->connect_error);
-    }else{
-        echo"Suksese";
+    private $host = "localhostt";
+    private $username = "roott";
+    private $password = "";
+    private $db = "web";
+
+function startConnection(){
+    try{
+        $conn = new PDO("mysql:host=$this->host;dbname=$this->db", $this->username);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        if(!$conn){
+            echo "Connection failed "; 
+           // return null;
+           echo"Suksese";
+        }else{
+            echo "Connection successful!"; 
+            return $conn;
+        }
+        
+    }catch(PDOException $e){
+        echo "Connection failed ". $e->getMessage();
+        return null;
     }
+    }
+}
+
+$db=new DatabaseConnection();
+$db->startConnection();
 
 ?>
